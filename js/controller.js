2529a661233b1a1f5c83a1c60d66ed813656b031
Jeado Ko <haibane84@gmail.com>
2012-10-09 10:21:00 +0900

    step2 finsh

--- js/controller.js
+++ js/controller.js
@@ -3,14 +3,12 @@
  *
  * Description
  */
-var todoApp = angular.module('todoApp', []);
+var todoApp = angular.module('todoApp', ['ngResource']);
 
-todoApp.controller('AppCtrl',function($scope){
-	var items = [
-		{text:'task 1+', done: false},
-		{text:'task 2+', done: false}
-	];
-
+todoApp.controller('AppCtrl',['$scope','Item', function($scope, Item){
+	
+	//fetch model
+	var items = Item.query();
 	$scope.items = items;
 
 	$scope.remaining = function(){
@@ -19,19 +17,43 @@ todoApp.controller('AppCtrl',function($scope){
 		}, 0);
 	};
 	
+	
 	$scope.add = function(newItem){
-		var item = {text: newItem.text};
+		var item = new Item({text: newItem.text});
 		items.push(item);
 		newItem.text = "";
-
-		console.log(window.newItemFrom.newItemText.$error);
+		item.$save();
 	};
 
 	$scope.archive = function(){
-		items=$scope.items = items.filter(function(item){
-			return !item.done;
+		items = $scope.items = items.filter(function(item){
+			if (item.done){
+				item.$remove();
+				return false;
+			}
+			return true;
 		});
 	};
+}]);
 
+todoApp.constant('apiKey', '50728d46e4b088be4c29ea02');
 
+todoApp.factory('Item',function($resource, apiKey){
+	var Item = $resource('https://api.mongolab.com/api/1/databases/angular-todo/collections/todos/:id',{
+		apiKey : apiKey
+	},{
+		update : {method:'PUT'}
+	});
+
+	Item.prototype.$remove = function(){
+		Item.remove({id: this._id.$oid});
+	};
+
+	Item.prototype.$update = function() {
+		return Item.update({id:this._id.$oid}, angular.extend({},this,{_id:undefined}));
+	};
+
+	Item.prototype.done = false;
+
+	return Item;
 });
